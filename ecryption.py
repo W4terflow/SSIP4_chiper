770a7f1b8aae1bc195e2f47c652a2948a6de7efe
W4terflow <ts.greenemteam@gmail.com>
2025-04-04 16:17:23 +0300

    better version

--- ecryption.py
+++ ecryption.py
@@ -1,44 +1,44 @@
+# Функция шифрования обобщенным шифром Цезаря
+def encrypt(k, m):
+    return ''.join(map(chr, [(ord(c) + k) % 65536 for c in m]))
+
+# Функция дешифрования обобщенным шифром Цезаря
+def decrypt(k, m):
+    return ''.join(map(chr, [(ord(c) - k) % 65536 for c in m]))
+
+# Функция взлома шифра Цезаря с использованием частотного анализа
 def break_cipher(text):
-    import string
+    from collections import Counter
 
-    # Создаем множество допустимых символов для русского языка
-    russian_letters = set(
-        'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' +
-        'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' +
-        string.punctuation + string.whitespace + '0123456789'
-    )
+    # Находим самый часто встречающийся символ в шифротексте
+    counter = Counter(text)
+    most_common_char, _ = counter.most_common(1)[0]
 
-    max_score = 0
-    decrypted_text = ''
-    probable_key = 0
+    # Код символа самого частого символа в шифротексте
+    cipher_most_common_code = ord(most_common_char)
 
-    for k in range(65536):
-        # Дешифруем текст с текущим ключом
-        attempted_text = ''.join(
-            [chr((ord(c) - k) % 65536) for c in text]
-        )
+    # Код символа пробела
+    space_code = ord(' ')
 
-        # Считаем количество допустимых символов в расшифрованном тексте
-        score = sum(char in russian_letters for char in attempted_text)
+    # Вычисляем предполагаемый ключ
+    key = (cipher_most_common_code - space_code) % 65536
 
-        # Если счет выше максимального, обновляем максимальный счет и сохраняем текст
-        if score > max_score:
-            max_score = score
-            decrypted_text = attempted_text
-            probable_key = k
+    # Дешифруем текст с найденным ключом
+    decrypted_text = decrypt(key, text)
 
-        # Опционально: вывести прогресс каждые N итераций
-        # if k % 10000 == 0:
-        #     print(f"Проверено ключей: {k}")
+    return decrypted_text, key
 
-        # Если максимально возможный счет достигнут, можно прервать цикл
-        if score == len(text):
-            break
+# Основная часть программы
+if __name__ == "__main__":
+    # Запрашиваем у пользователя ключ и текст для шифрования
+    key = int(input("Введите ключ для шифрования (целое число): "))
+    plain_text = input("Введите текст для шифрования: ")
 
-    print(f"Найденный ключ: {probable_key}")
-    return decrypted_text
+    # Шифруем текст
+    encrypted_text = encrypt(key, plain_text)
+    print("\nЗашифрованный текст:\n", encrypted_text)
 
-# Пример использования функции
-cipher_text = input("Введите зашифрованный текст: ")
-original_text = break_cipher(cipher_text)
-print("Расшифрованный текст:", original_text)
+    # Взламываем шифр без знания ключа
+    recovered_text, recovered_key = break_cipher(encrypted_text)
+    print("\nВосстановленный текст без знания ключа:\n", recovered_text)
+    print("\nНайденный ключ:", recovered_key)
